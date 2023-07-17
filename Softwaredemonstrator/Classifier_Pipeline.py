6b0141ba4fad747ab75e762536d271945f31e357
Marco Hussong <mhussong@rhrk.uni-kl.de>
2023-07-17 15:24:44 +0200

    New Classifier Model and removal of stl files

--- Softwaredemonstrator/Classifier_Pipeline.py
+++ Softwaredemonstrator/Classifier_Pipeline.py
@@ -2,12 +2,15 @@ from utils import binvox_rw
 import numpy as np
 import torch
 
-model_Drahterodieren = torch.jit.load('./model/model_DE_2023-06-22_19-44-33.pt') # map_location=torch.device('cuda:0'), if model should be loaded to GPU, by default CPU
-model_Fraesen_Drehen = torch.jit.load('./model/model_F&D_2023-06-23_12-46-35.pt') # map_location=torch.device('cuda:0'), if model should be loaded to GPU, by default CPU
-model_Schleifen = torch.jit.load('./model/model_Sch_2023-06-22_19-44-33.pt')
-model_Drahterodieren.eval()
-model_Fraesen_Drehen.eval()
-model_Schleifen.eval()
+# model_Drahterodieren = torch.jit.load('./model/model_DE_2023-06-22_19-44-33.pt') # map_location=torch.device('cuda:0'), if model should be loaded to GPU, by default CPU
+# model_Fraesen_Drehen = torch.jit.load('./model/model_F&D_2023-06-23_12-46-35.pt') # map_location=torch.device('cuda:0'), if model should be loaded to GPU, by default CPU
+# model_Schleifen = torch.jit.load('./model/model_Sch_2023-06-22_19-44-33.pt')
+# model_Drahterodieren.eval()
+# model_Fraesen_Drehen.eval()
+# model_Schleifen.eval()
+
+model_class_all = torch.jit.load('./model/model_2023-07-14_05-20-22_M.pt')
+model_class_all.eval()
 
 
 def classification(voxelFilePath):
@@ -25,32 +28,49 @@ def classification(voxelFilePath):
     # voxel = voxel.astype(np.float32)
     # voxel = torch.tensor(voxel)
 
-    with torch.no_grad():
-        output = model_Drahterodieren(voxel)
-        print(output)
-        output = torch.round(output)
-        output = torch.squeeze(output)
-        if output.item() == 1:
-            List.append('Drahterodieren')
+    # with torch.no_grad():
+    #     output = model_Drahterodieren(voxel)
+    #     print(output)
+    #     output = torch.round(output)
+    #     output = torch.squeeze(output)
+    #     if output.item() == 1:
+    #         List.append('Drahterodieren')
+
+    # with torch.no_grad():
+    #     output = model_Fraesen_Drehen(voxel)
+    #     output = torch.round(output)
+    #     output = torch.squeeze(output)
+    #     if output[0] == 1:
+    #         List.append('Fräsen')
+    #     if output[1] == 1:
+    #         List.append('Drehen')
+
+    # with torch.no_grad():
+    #     output = model_Schleifen(voxel)
+    #     output = torch.round(output)
+    #     output = torch.squeeze(output)
+    #     if output[0] == 1:
+    #         List.append('Flachschleifen')
+    #     if output[1] == 1:
+    #         List.append('Rundschleifen')
+    #     if output[2] == 1:
+    #         List.append('Koordinatenschleifen')
 
     with torch.no_grad():
-        output = model_Fraesen_Drehen(voxel)
+        output = model_class_all(voxel)
         output = torch.round(output)
         output = torch.squeeze(output)
         if output[0] == 1:
             List.append('Fräsen')
         if output[1] == 1:
             List.append('Drehen')
-
-    with torch.no_grad():
-        output = model_Schleifen(voxel)
-        output = torch.round(output)
-        output = torch.squeeze(output)
-        if output[0] == 1:
-            List.append('Flachschleifen')
-        if output[1] == 1:
-            List.append('Rundschleifen')
         if output[2] == 1:
+            List.append('Drahterodieren')
+        if output[3] == 1:
+            List.append('Flachschleifen')
+        if output[4] == 1:
+            List.append('Rundschleifen')
+        if output[5] == 1:
             List.append('Koordinatenschleifen')
 
     return List
